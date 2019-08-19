c8434886ce22cd26d5fc5d03a415a11d9c5ad6d3
Martin <galimwavu@users.noreply.github.com>
2019-08-19 18:34:42 +0300

    Create ip_validation.py

---
+++ ip_validation.py
@@ -0,0 +1,12 @@
+#validate the ip adress
+def validate_ip(ipaddress):
+    a = ipaddress.split('.')
+    if len(a) != 4:
+        return False
+    for x in a:
+        if not x.isdigit():
+            return False
+        i = int(x)
+        if i < 0 or i > 255:
+            return False
+    return True
